4429c10175da270a068c64e1e81fca5fa3baea54
Kiana Nasiri <101814833+kiananasiri@users.noreply.github.com>
2025-03-07 22:52:18 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+echo "DOMAIN is set to: $DOMAIN"
+
+if [ -z "$DOMAIN" ]; then
+  echo "ERROR: DOMAIN environment variable is not set!"
+  exit 1
+fi
+
 envsubst '$DOMAIN' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf  # Debug output
 nginx -g 'daemon off;'
