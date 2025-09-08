2b10910674f01c14b0bd1bc68d6035134ee9682d
desxtina <desmohamadi03@gmail.com>
2025-09-08 11:06:36 +0330

    flushed old DB, added signals.py as seeder, populated all entities

--- backend/therapy/apps.py
+++ backend/therapy/apps.py
@@ -6,5 +6,5 @@ class TherapyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'therapy'
 
-    #def ready(self):
-        #import signals
+    def ready(self):
+        from . import signals
